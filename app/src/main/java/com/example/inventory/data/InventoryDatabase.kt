800a5f3678fcf95b943809683cd7886b0c183a8f
Grigory <lalaevgri@gmail.com>
2023-12-01 22:33:41 +0300

    fix database migration

--- app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
+++ app/src/main/java/com/example/inventory/data/InventoryDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Item::class], version = 1, exportSchema = false)
+@Database(entities = [Item::class], version = 2, exportSchema = false)
 abstract class InventoryDatabase : RoomDatabase() {
     abstract fun itemDao(): ItemDao
     companion object {
@@ -15,6 +15,7 @@ abstract class InventoryDatabase : RoomDatabase() {
         fun getDatabase(context: Context): InventoryDatabase {
             return Instance ?: synchronized(this) {
                 Room.databaseBuilder(context, InventoryDatabase::class.java, "item_database")
+                    .fallbackToDestructiveMigration()
                     .build()
                     .also { Instance = it }
             }
